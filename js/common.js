e516b5acb3a1b264ffb0b54a41d3c566df696475
Georgiy <goshazvir@gmail.com>
2014-04-26 17:36:31 +0300

    load

--- js/common.js
+++ js/common.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
+	$('.js-btn-popup1').click(function(event) {
+		$('.js-popup1').addClass('is-active');
+		$('.overlay').addClass('is-active');
+		return false;
+	});
+	$('.js-btn-popup2').click(function(event) {
+		$('.js-popup2').addClass('is-active');
+		$('.overlay').addClass('is-active');
+		return false;
+	});
+	$('.popup__close, .overlay').click(function(event) {
+		$('.popup').removeClass('is-active');
+		$('.overlay').removeClass('is-active');
+		return false;
+	});
 
 });
